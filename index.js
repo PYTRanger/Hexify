ad43e6982bfe49d3145b0d8ef74c4e22143457c4
Utkarsh Jaiswal <1406utkarsh@gmail.com>
2024-03-17 02:12:35 +0530

    Revert "Yogitaa"

--- index.js
+++ index.js
@@ -100,12 +100,6 @@ function runExternalProcess(command) {
     child.on('error', (err) => {
         console.error(`Error launching ${command}: ${err}`);
     });
-  }
-// Handle reminder creation
-ipcMain.on('createReminder', (event, reminder) => {
-    console.log('Reminder created:', reminder);
-    // Here you can implement the logic to save the reminder
-  });
 
     child.on('exit', (code) => {
         if (code !== 0) {
@@ -113,6 +107,7 @@ ipcMain.on('createReminder', (event, reminder) => {
         }
     });
 }
+
 module.exports = {
     createMainWindow,
     runExternalProcess
